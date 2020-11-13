4d1a3b08663a5a777dca61c3fde28b3ffdbbfd6d
ester-naranjo-rodrigo <esthernaran@gmail.com>
2020-11-13 16:17:19 +0100

    primercom

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kre4e)=9b&g1#7crihf+0c6^5-8nct+!ahj-bi*f+i6$n($h(q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
