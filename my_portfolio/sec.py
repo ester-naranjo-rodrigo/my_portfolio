93d95161ed05aa04b737dd901a9f03dbd99f4fd5
ester-naranjo-rodrigo <esthernaran@gmail.com>
2020-11-15 22:49:40 +0100

    primercom

--- my_portfolio/sec.py
+++ my_portfolio/sec.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DEBUG = True
 SECRET_KEY = 'kre4e)=9b&g1#7crihf+0c6^5-8nct+!ahj-bi*f+i6$n($h(q'
 DATABASES = {
