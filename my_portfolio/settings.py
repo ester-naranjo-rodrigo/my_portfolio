a2cb943e0c7a322ff7cf452c0fb0c4b711f93839
ester-naranjo-rodrigo <esthernaran@gmail.com>
2020-11-15 22:43:51 +0100

    primercom

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- my_portfolio/views.py
+++ my_portfolio/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-import datetime
 
 def homepage(request):
     return render(request, 'my_portfolio/homepage.html')
